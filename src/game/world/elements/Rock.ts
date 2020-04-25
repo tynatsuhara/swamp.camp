5d5d7d3229970402d33e4b8c4b2ca67e7710803c
Tyler Bonnell <tbonnell@uw.edu>
2020-04-25 11:41:29 -0700

    actually add rox

---
+++ src/game/world/elements/Rock.ts
@@ -0,0 +1,30 @@
+import { Point } from "../../../engine/point"
+import { TILE_SIZE, Tilesets } from "../../graphics/Tilesets"
+import { BoxCollider } from "../../../engine/collision/BoxCollider"
+import { WorldLocation } from "../WorldLocation"
+import { TileComponent } from "../../../engine/tiles/TileComponent"
+import { TileTransform } from "../../../engine/tiles/TileTransform"
+import { Entity } from "../../../engine/Entity"
+
+
+export const makeRock = (wl: WorldLocation, pos: Point) => {
+    const e = new Entity()
+    const variation = Math.floor(Math.random() * 3) + 1
+    const mossy = Math.random() > .7
+
+    const tile = e.addComponent(new TileComponent(
+        Tilesets.instance.outdoorTiles.getTileSource(`rock${variation}${mossy ? 'mossy' : ''}`), 
+        new TileTransform(pos.times(TILE_SIZE))
+    ))
+    tile.transform.depth = (pos.y + 1) * TILE_SIZE
+    tile.transform.mirrorX = Math.random() > .5
+
+    // TODO
+    const hitboxDims = new Point(12, 4)
+    e.addComponent(new BoxCollider(
+        pos.plus(new Point(.5, 1)).times(TILE_SIZE).minus(new Point(hitboxDims.x/2, hitboxDims.y + 2)), 
+        hitboxDims
+    ))
+
+    wl.stuff.set(pos, e)
+}
