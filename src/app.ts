de75fdf291d4400e7c88254a616cb5901641d77d
Tyler Bonnell <tbonnell@uw.edu>
2021-04-24 12:04:22 -0700

    reformat app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,12 @@ import { Engine } from "./engine/engine"
 import { Tilesets } from "./game/graphics/Tilesets"
 import { assets } from "./engine/Assets"
 
-assets.loadImageFiles(Tilesets.getFilesToLoad()).then(() => {
-    new Engine(new QuestGame(), <HTMLCanvasElement>document.getElementById('canvas'))
+Promise.all([
+    assets.loadImageFiles(Tilesets.getFilesToLoad()),
+]).then(() => {
+    new Engine(
+        new QuestGame(), 
+        <HTMLCanvasElement>document.getElementById('canvas')
+    )
 })
 
