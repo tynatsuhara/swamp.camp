f75a335a45c87b6712c34e6215c9e171ae26a22b
Tyler Bonnell <tbonnell@uw.edu>
2021-06-08 20:12:00 -0700

    squircles

--- src/game/world/Vignette.ts
+++ src/game/world/Vignette.ts
@@ -8,10 +8,9 @@ import { MapGenerator } from "./MapGenerator"
 
 export class Vignette extends Component {
 
-    private padding = 128
+    private padding = 128  // distance from start of shadows to the edge of the screen
     private rings = 8
     private ringWidth = this.padding/this.rings
-    private readonly R4 = Math.pow(MapGenerator.MAP_SIZE/2 * TILE_SIZE, 4)
 
     constructor(topLeftPosition: Point, diameter: number) {
         super()
@@ -23,24 +22,22 @@ export class Vignette extends Component {
             const rgb = getRGB(Color.BLACK)
             
             const center = new Point(diameter/2, diameter/2).apply(Math.floor)
-            for (let x = 0; x < diameter; x++) {
-                for (let y = 0; y < diameter; y++) {
-                    const i = (x + y * diameter) * 4
-                    const pt = new Point(x, y)
-                    const dist = pt.distanceTo(center)
-                    // how far into the darkness is it?
-                    const distFromLightEdge = dist - (diameter/2 - this.padding)
-                    if (distFromLightEdge > 0) {
-                        // TODO: support squircle vignette
-                        // if (xa + yb > this.R4) {
-                        // const xa = Math.pow(x - diameter/2, 4)
-                        // const yb = Math.pow(y - diameter/2, 4)
-                        // const ring = 8
-                        const ring = Math.min(Math.floor(distFromLightEdge/this.ringWidth), this.rings)
-                        imageData.data[i+0] = rgb[0]
-                        imageData.data[i+1] = rgb[1]
-                        imageData.data[i+2] = rgb[2]
-                        imageData.data[i+3] = 255 * ring/this.rings * .95
+            for (let ring = 0; ring < this.rings; ring++) {
+                const n = 4  // superellipse n parameter
+                const radius = MapGenerator.MAP_SIZE/2 * TILE_SIZE - ((this.rings - ring - 1) * this.ringWidth)
+                const r = Math.pow(radius, n)
+
+                for (let x = 0; x < diameter; x++) {
+                    for (let y = 0; y < diameter; y++) {
+                        const i = (x + y * diameter) * 4
+                        const xa = Math.pow(x - diameter/2, n)
+                        const yb = Math.pow(y - diameter/2, n)
+                        if (xa + yb > r) {
+                            imageData.data[i+0] = rgb[0]
+                            imageData.data[i+1] = rgb[1]
+                            imageData.data[i+2] = rgb[2]
+                            imageData.data[i+3] = 255 * (ring+1)/this.rings * .95
+                        }
                     }
                 }
             }
