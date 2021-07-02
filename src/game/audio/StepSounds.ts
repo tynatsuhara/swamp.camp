39c77db8ae08e0c14dbd4e320924c1f9947397a6
Tyler Bonnell <tbonnell@uw.edu>
2021-07-01 19:25:12 -0700

    add water sounds

--- src/game/audio/StepSounds.ts
+++ src/game/audio/StepSounds.ts
@@ -19,6 +19,10 @@ export class StepSounds {
     private static readonly WOOD_1 = "audio/steps/wood01.ogg"
     private static readonly WOOD_2 = "audio/steps/wood02.ogg"
     private static readonly WOOD_3 = "audio/steps/wood03.ogg"
+    private static readonly WATER_1 = "audio/steps/wave_01.flac"
+    private static readonly WATER_2 = "audio/steps/wave_02.flac"
+    private static readonly WATER_3 = "audio/steps/wave_03.flac"
+    private static readonly WATER_4 = "audio/steps/wave_04.flac"
 
     static startFootstepSoundLoop = () => {
         assets.loadAudioFiles([ 
@@ -29,7 +33,11 @@ export class StepSounds {
             StepSounds.STONE, 
             StepSounds.WOOD_1, 
             StepSounds.WOOD_2, 
-            StepSounds.WOOD_3 
+            StepSounds.WOOD_3,
+            StepSounds.WATER_1,
+            StepSounds.WATER_2,
+            StepSounds.WATER_3,
+            StepSounds.WATER_4,
         ]).then(() => StepSounds.doFootstep())
     }
     
@@ -64,14 +72,22 @@ export class StepSounds {
             case GroundType.GRASS:
             case GroundType.LEDGE:
                 return Lists.oneOf([
-                    [StepSounds.LEAVES_1, .2],
-                    [StepSounds.LEAVES_2, .2],
+                    [StepSounds.LEAVES_1, .3],
+                    [StepSounds.LEAVES_2, .3],
                 ])
             case GroundType.BASIC:
             case GroundType.BASIC_NINE_SLICE:
-                return [StepSounds.WOOD_1, .15]
+                return [StepSounds.WOOD_1, .3]
             case GroundType.PATH:
-                return [StepSounds.MUD, .15]
+                return [StepSounds.MUD, .3]
+            case GroundType.WATER:
+                const waterVolume = .035
+                return Lists.oneOf([
+                    [StepSounds.WATER_1, waterVolume],
+                    [StepSounds.WATER_2, waterVolume],
+                    [StepSounds.WATER_3, waterVolume],
+                    [StepSounds.WATER_4, waterVolume],
+                ])
             default:
                 console.log("no mapped sound for ground type")
                 return [undefined, 0]
