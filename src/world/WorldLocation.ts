6af7d91dd346079fd3c27da0852b3b0dacfc49fb
Tyler Bonnell <tbonnell@uw.edu>
2021-07-31 13:03:16 -0700

    make water more expensive to traverse

--- src/world/WorldLocation.ts
+++ src/world/WorldLocation.ts
@@ -209,6 +209,7 @@ export class WorldLocation {
         
         return this.occupied.findPath(tileStart, tileEnd, {
             heuristic: (pt) => heuristic(pt, tileEnd),
+            distance: (a, b) => Ground.isWater(this.getGround(b)?.type) ? 3 : 1,
             isOccupied: (pt) => {
                 // Assuming this is used for character-to-character pathfinding, the start
                 // and end points in the grid should be assumed to be open. For instance,
