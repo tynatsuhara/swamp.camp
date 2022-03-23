80c0a02aab0f26db41b598eeeacd3d8bf48e2444
Tyler Bonnell <tbonnell@uw.edu>
2022-03-22 21:14:22 -0700

    apothecary interior updates

--- src/world/buildings/Apothecary.ts
+++ src/world/buildings/Apothecary.ts
@@ -10,7 +10,7 @@ import { NavMeshCollider } from "../elements/NavMeshCollider"
 import { Location } from "../Location"
 import { LocationManager, LocationType } from "../LocationManager"
 import { SingleTypeResidence } from "../residences/SingleTypeResidence"
-import { Teleporter, TeleporterPrefix } from "../Teleporter"
+import { TeleporterPrefix } from "../Teleporter"
 import { BuildingFactory } from "./Building"
 import { InteriorUtils } from "./InteriorUtils"
 
@@ -102,21 +102,28 @@ const makeApothecaryInterior = (outside: Location): Location => {
     const l = new Location(LocationType.APOTHECARY_INTERIOR, true, false)
 
     LocationManager.instance.add(l)
-    const dimensions = new Point(4, 4)
-    const interactablePos = new Point(1, 4).times(TILE_SIZE)
-    const teleporter: Teleporter = {
+    const dimensions = new Point(4, 3)
+    const interactablePos = new Point(1.5, 3).times(TILE_SIZE)
+    l.setBarriers(InteriorUtils.makeBarriers(dimensions))
+    l.addTeleporter({
         to: outside.uuid,
         pos: interactablePos.plusY(-4),
         id: TeleporterPrefix.DOOR,
-    }
-
-    l.setBarriers(InteriorUtils.makeBarriers(dimensions))
-    l.addTeleporter(teleporter)
+    })
+    l.addElement(ElementType.TELEPORTER_INDICATOR, new Point(1, 3), {
+        to: outside.uuid,
+        i: interactablePos.toString(),
+        id: TeleporterPrefix.DOOR,
+    })
 
     l.sprites.addSprite("dr-interior", new Point(0, -TILE_SIZE * 2), 0, Number.MIN_SAFE_INTEGER)
-    // l.sprites.addSprite("skeleton", new Point(0, -TILE_SIZE * 2), 0)
 
-    l.sprites.addSprite("dr-counter", new Point(TILE_SIZE, TILE_SIZE), 0, TILE_SIZE)
+    const skeletonPos = new Point(3, 1).times(TILE_SIZE)
+    l.sprites.addSprite("skeleton", skeletonPos, 0, skeletonPos.y + TILE_SIZE * 2 - 4)
+
+    // TODO: add counter collider..... this is annoying
+    const counterPos = new Point(TILE_SIZE, 7)
+    l.sprites.addSprite("dr-counter", counterPos, 0, counterPos.y + TILE_SIZE)
 
     return l
 }
