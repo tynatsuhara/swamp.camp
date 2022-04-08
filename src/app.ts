745579d33c05de7a22560260e2653d68ab87cfc8
Tyler Bonnell <tbonnell@uw.edu>
2022-04-08 15:05:26 -0700

    update woff link for special characters

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ Promise.all([
     assets.loadImageFiles(getFilesToLoadForMainMenu()),
     assets.loadFont(
         TEXT_FONT,
-        "url(https://fonts.gstatic.com/s/pressstart2p/v9/e3t4euO8T-267oIAQAu6jDQyK3nVivM.woff2)"
+        "url(https://fonts.gstatic.com/l/font?kit=e3t4euO8T-267oIAQAu6jDQyK0nXgPRE4rWoBrU8zk58VDOks2wfaGBt3jvARHQcUjfUQlapyTOIDwisBxCS_Zd0xsD9mIZYb1tPZk2A3HROJNZNzLR9wtR9EVHXnVspl-6RIkwvJ_s3MWoB8LxL9JhapdYRCjmPjczWE-9YtPT1acz85miar2ZBC8tfqcw-tsaThZS0xXDNw2zo9fY0ZgJycybMEEx7qYEfz0rWH4hiv1HEe6Lex3p7dnKW4VhSG9_Rnvip9iWoLZIId3-Wy1fI1ERBdWPRGUHXDmEaY3xtzyr4yu7U6qX1OqbPVmJKPfmgB-CWF7aMO9yL8S1JlPgi69EQTgrVUKP5VG-vpgAbX1PudUlHQHpiM9dfHvKfIfzP2WqL5ZLvP4iNShfeF15g4_GWSuubnxy0oYLW00-J55DNjaCdehEkBiE3SAR-BcK3hw-Kf6KYOs5SySX6XJ3bKfuu-Q0FOyKgLUCHa6qsZYwyMaYW73RlJxowNBRPs2Tkc91v6cUz9JHfBE0LwVmBcCy81GHXq3AVJvGC6A_tXlZLCiiVVCFA75-JGlzte4mX82eHGhHO&skey=24cc298767068e24&v=v12)"
     ),
 ]).then(() => {
     // start the engine (vroom vroom)
