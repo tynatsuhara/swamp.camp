d0fdce6209f6ca5e04a32321090cf2e8a36972c3
Tyler Bonnell <tbonnell@uw.edu>
2022-04-07 19:30:20 -0700

    make it a bit louder

--- src/ui/UISounds.ts
+++ src/ui/UISounds.ts
@@ -5,5 +5,5 @@ const CLICK_SOUND = "audio/ui/switch22.ogg"
 
 export const UISounds = {
     loadAll: () => assets.loadAudioFiles([CLICK_SOUND]),
-    playClickSound: () => Sounds.play(CLICK_SOUND, 0.03),
+    playClickSound: () => Sounds.play(CLICK_SOUND, 0.035),
 }
