7bcd6c35c463d119880a363ca7c518b6b7dbc0dc
Tyler Bonnell <tbonnell@uw.edu>
2022-06-22 21:24:53 -0700

    simplify church

--- src/world/buildings/Church.ts
+++ src/world/buildings/Church.ts
@@ -1,98 +1,25 @@
-import { Entity } from "brigsby/dist/Entity"
 import { Point } from "brigsby/dist/Point"
-import { SpriteTransform } from "brigsby/dist/sprites/SpriteTransform"
 import { DudeType } from "../../characters/DudeType"
-import { Tilesets, TILE_SIZE } from "../../graphics/Tilesets"
-import { ElementComponent } from "../elements/ElementComponent"
+import { TILE_SIZE } from "../../graphics/Tilesets"
 import { ElementType } from "../elements/Elements"
-import { Interactable } from "../elements/Interactable"
-import { NavMeshCollider } from "../elements/NavMeshCollider"
 import { GroundType } from "../ground/Ground"
 import { Location } from "../Location"
 import { LocationManager, LocationType } from "../LocationManager"
-import { SingleTypeResidence } from "../residences/SingleTypeResidence"
 import { Teleporter, TeleporterPrefix } from "../Teleporter"
 import { AsciiInteriorBuilder } from "./AsciiInteriorBuilder"
-import { BuildingFactory } from "./Building"
 import { InteriorUtils } from "./InteriorUtils"
+import { SimpleBuildingFactory } from "./SimpleBuildingFactory"
 
-type ChurchData = {
-    interiorUUID: string
-    nuns: string[]
-    clerics: string[]
-    bishops: string[]
-}
-
-export class ChurchFactory extends BuildingFactory {
-    readonly type = ElementType.CHURCH
-    readonly dimensions = new Point(5, 4)
-
-    make(wl: Location, pos: Point, data: ChurchData): ElementComponent {
-        const e = new Entity()
-
-        // the interior location UUID
-        const interiorUUID: string = data.interiorUUID ?? makeChurchInterior(wl).uuid
-
-        // spriteTilePos accounts for 1 tile of space on sides and the roof
-        const spriteTilePos = pos.plus(new Point(1, -2))
-        const interactablePos = spriteTilePos.plus(new Point(1.5, 5)).times(TILE_SIZE)
-        const doorId = TeleporterPrefix.DOOR
-
-        // TODO: Can we combine this with the interactable step below?
-        wl.addTeleporter({
-            to: interiorUUID,
-            pos: interactablePos.plusY(12),
-            id: doorId,
-        })
-
-        const depth = (spriteTilePos.y + 5) * TILE_SIZE
-
-        // Set up sprite
-        e.addComponent(
-            Tilesets.instance.largeSprites.getTileSource("church").toComponent(
-                SpriteTransform.new({
-                    position: spriteTilePos.times(TILE_SIZE),
-                    depth,
-                })
-            )
+export class ChurchFactory extends SimpleBuildingFactory {
+    constructor() {
+        super(
+            ElementType.CHURCH,
+            new Point(5, 4),
+            "church",
+            makeChurchInterior,
+            new Point(TILE_SIZE * 3, TILE_SIZE * 2),
+            { [DudeType.NUN]: 3, [DudeType.CLERIC]: 2, [DudeType.BISHOP]: 1 }
         )
-
-        // Set up collider
-        e.addComponent(
-            new NavMeshCollider(
-                wl,
-                spriteTilePos.plusY(3).times(TILE_SIZE),
-                new Point(TILE_SIZE * 3, TILE_SIZE * 2)
-            )
-        )
-
-        // Set up teleporter
-        e.addComponent(
-            new Interactable(
-                interactablePos,
-                () => wl.playerUseTeleporter(interiorUUID, doorId),
-                new Point(0, -TILE_SIZE * 1.4)
-            )
-        )
-
-        const nunsRez = e.addComponent(
-            new SingleTypeResidence(DudeType.NUN, 3, interiorUUID, data.nuns || [])
-        )
-        const clericsRez = e.addComponent(
-            new SingleTypeResidence(DudeType.CLERIC, 3, interiorUUID, data.clerics || [])
-        )
-        const bishopsRez = e.addComponent(
-            new SingleTypeResidence(DudeType.BISHOP, 1, interiorUUID, data.bishops || [])
-        )
-
-        const save: () => ChurchData = () => ({
-            interiorUUID,
-            nuns: nunsRez.getResidents(),
-            clerics: clericsRez.getResidents(),
-            bishops: bishopsRez.getResidents(),
-        })
-
-        return e.addComponent(new ElementComponent(this.type, pos, save))
     }
 }
 
