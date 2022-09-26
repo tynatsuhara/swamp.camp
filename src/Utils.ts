a59bf421a56415cadc19b82daa7807c459417896
Tyler Bonnell <tbonnell@uw.edu>
2022-09-25 21:12:33 -0700

    add bounding box util

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,4 +1,5 @@
 import { Point } from "brigsby/dist"
+import { ImageRender, LineRender } from "brigsby/dist/renderer"
 
 export const tilesAround = (center: Point, radius: number = 1): Point[] => {
     let result = []
@@ -12,4 +13,16 @@ export const tilesAround = (center: Point, radius: number = 1): Point[] => {
     }
 
     return result
+}
+
+export const showBoundingBox = (sprite: ImageRender): LineRender[] => {
+    const { x: width, y: height } = sprite.dimensions
+    return [
+        [new Point(0, 0), new Point(width, 0)], // top
+        [new Point(0, height), new Point(width, height)], // bottom
+        [new Point(0, 0), new Point(0, height)], // left
+        [new Point(width, 0), new Point(width, height)], // right
+    ].map(([start, end]) => {
+        return new LineRender(start.plus(sprite.position), end.plus(sprite.position))
+    })
 }
