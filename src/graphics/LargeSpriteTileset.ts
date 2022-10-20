3eee2281fde976194d5818073e722d06a20bedf1
Tyler Bonnell <tbonnell@uw.edu>
2022-10-19 18:57:48 -0700

    add tent-interior sprite

--- src/graphics/LargeSpriteTileset.ts
+++ src/graphics/LargeSpriteTileset.ts
@@ -1,4 +1,4 @@
-import { Point } from "brigsby/dist"
+import { Point, pt } from "brigsby/dist"
 import { StaticSpriteSource } from "brigsby/dist/sprites"
 import { getImage, TILE_SIZE } from "./Tilesets"
 
@@ -6,19 +6,21 @@ export class LargeSpriteTileset {
     getTileSource(key: string): StaticSpriteSource {
         switch (key) {
             case "mine-small":
-                return this.get(Point.ZERO, new Point(5, 5))
+                return this.get(Point.ZERO, pt(5, 5))
             case "church":
-                return this.get(new Point(16, 0), new Point(3, 5))
+                return this.get(pt(16, 0), pt(3, 5))
             case "dr-interior":
-                return this.get(new Point(1, 10), new Point(4, 5))
+                return this.get(pt(1, 10), pt(4, 5))
             case "dr-counter":
-                return this.get(new Point(0, 15), new Point(3, 1))
+                return this.get(pt(0, 15), pt(3, 1))
             case "skeleton":
-                return this.get(new Point(0, 11), new Point(1, 2))
+                return this.get(pt(0, 11), pt(1, 2))
             case "apothecary":
-                return this.get(new Point(6, 10), new Point(3, 5))
+                return this.get(pt(6, 10), pt(3, 5))
             case "cabin-small":
-                return this.get(new Point(0, 6), new Point(3, 3))
+                return this.get(pt(0, 6), pt(3, 3))
+            case "tent-interior":
+                return this.get(pt(0, 22), pt(5, 7))
             default:
                 throw new Error(`${key} is not a valid tile`)
         }
