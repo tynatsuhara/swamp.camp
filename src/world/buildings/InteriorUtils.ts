a5f186739dd372cbc9ff61481a2e0491d7828e2c
Tyler Bonnell <tbonnell@uw.edu>
2022-10-21 19:49:03 -0700

    remove old barrier impl

--- src/world/buildings/InteriorUtils.ts
+++ src/world/buildings/InteriorUtils.ts
@@ -1,6 +1,5 @@
 import { Point } from "brigsby/dist"
 import { TILE_SIZE } from "../../graphics/Tilesets"
-import { Barrier } from "../Barrier"
 import { Location } from "../locations/Location"
 
 const BARRIER_WIDTH = 30
@@ -8,31 +7,6 @@ const SIDE_PADDING = 7
 const TOP_BOT_PADDING = 3
 
 export const InteriorUtils = {
-    makeBarriers(tileDimensions: Point) {
-        return [
-            // left
-            new Barrier(
-                new Point(-BARRIER_WIDTH + SIDE_PADDING, -BARRIER_WIDTH),
-                new Point(BARRIER_WIDTH, tileDimensions.y * TILE_SIZE + 2 * BARRIER_WIDTH)
-            ),
-            // right
-            new Barrier(
-                new Point(tileDimensions.x * TILE_SIZE - SIDE_PADDING, -BARRIER_WIDTH),
-                new Point(BARRIER_WIDTH, tileDimensions.y * TILE_SIZE + 2 * BARRIER_WIDTH)
-            ),
-            // top
-            new Barrier(
-                new Point(-BARRIER_WIDTH, -BARRIER_WIDTH + TOP_BOT_PADDING),
-                new Point(tileDimensions.x * TILE_SIZE + 2 * BARRIER_WIDTH, BARRIER_WIDTH)
-            ),
-            // bottom
-            new Barrier(
-                new Point(-BARRIER_WIDTH, tileDimensions.y * TILE_SIZE - TOP_BOT_PADDING),
-                new Point(tileDimensions.x * TILE_SIZE + 2 * BARRIER_WIDTH, BARRIER_WIDTH)
-            ),
-        ]
-    },
-
     addBarriers(l: Location, tileDimensions: Point) {
         const verticalBarrier = {
             y: -BARRIER_WIDTH,
