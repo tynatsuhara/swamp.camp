170976b1be8eaeb5cd1bb3d576d5f447e04dfb9e
Tyler Bonnell <tbonnell@uw.edu>
2022-10-26 17:42:40 -0700

    v0 mine particles

--- src/world/buildings/MineExit.ts
+++ src/world/buildings/MineExit.ts
@@ -1,5 +1,8 @@
-import { Entity, Point } from "brigsby/dist"
+import { Entity, Point, pt } from "brigsby/dist"
+import { Lists, RepeatedInvoker } from "brigsby/dist/util"
+import { Particles } from "../../graphics/particles/Particles"
 import { TILE_SIZE } from "../../graphics/Tilesets"
+import { Color } from "../../ui/Color"
 import { ElementComponent } from "../elements/ElementComponent"
 import { ElementFactory } from "../elements/ElementFactory"
 import { ElementType } from "../elements/Elements"
@@ -30,6 +33,26 @@ export class MineExitFactory extends ElementFactory<ElementType.MINE_EXIT> {
                 () => wl.playerUseTeleporter(destinationUUID, id),
                 new Point(0, -TILE_SIZE)
             )
+        )
+
+        // TODO: pre-populate the area so particles are present when entered
+        e.addComponent(
+            new RepeatedInvoker(() => {
+                // create a particle
+                const pos = new Point(5, 3)
+                    .apply((n) => Math.random() * n * TILE_SIZE)
+                    .plusY(-2 * TILE_SIZE) // align with top
+                Particles.instance.emitParticle(
+                    Lists.oneOf([Color.BLACK, Color.TAUPE_1, Color.TAUPE_2]),
+                    pos,
+                    1_000_000_000,
+                    10_000,
+                    (t) => pt(0, t * 0.004),
+                    pt(Math.random() > 0.5 ? 1 : 2)
+                )
+
+                return 500 + Math.random() * 500
+            })
         )
 
         return e.addComponent(new ElementComponent(ElementType.MINE_EXIT, pos, () => data))
