28b1b78c6aaade19478e64616320f818145bf101
Tyler Bonnell <tbonnell@uw.edu>
2022-10-26 17:54:27 -0700

    spawn particles better

--- src/world/buildings/MineExit.ts
+++ src/world/buildings/MineExit.ts
@@ -1,4 +1,4 @@
-import { Entity, Point, pt } from "brigsby/dist"
+import { Component, Entity, Point, pt } from "brigsby/dist"
 import { Lists, RepeatedInvoker } from "brigsby/dist/util"
 import { Particles } from "../../graphics/particles/Particles"
 import { TILE_SIZE } from "../../graphics/Tilesets"
@@ -8,6 +8,7 @@ import { ElementFactory } from "../elements/ElementFactory"
 import { ElementType } from "../elements/Elements"
 import { Interactable } from "../elements/Interactable"
 import { Location } from "../locations/Location"
+import { WorldTime } from "../WorldTime"
 
 export class MineExitFactory extends ElementFactory<ElementType.MINE_EXIT> {
     readonly dimensions = new Point(1, 1)
@@ -27,33 +28,54 @@ export class MineExitFactory extends ElementFactory<ElementType.MINE_EXIT> {
         const interactPos = Point.fromString(i)
         const id = data.id
 
+        const spawnParticle = (lifetime = 10_000) => {
+            // create a particle
+            const pos = new Point(5, 3)
+                .apply((n) => Math.random() * n * TILE_SIZE)
+                .plusY(-1.5 * TILE_SIZE) // align with top
+            Particles.instance.emitParticle(
+                Lists.oneOf([Color.BLACK, Color.TAUPE_1, Color.TAUPE_2]),
+                pos,
+                1_000_000_000,
+                lifetime,
+                (t) => pt(0, t * 0.004),
+                pt(Math.random() > 0.5 ? 1 : 2)
+            )
+
+            return 500 + Math.random() * 500
+        }
+
+        let spawnParticlesAtTime = Number.MAX_SAFE_INTEGER
+
+        e.addComponent(
+            new Component({
+                start: () => {
+                    spawnParticlesAtTime = 0
+                },
+                update: () => {
+                    if (WorldTime.instance.time > spawnParticlesAtTime) {
+                        for (let i = 0; i < 10; i++) {
+                            spawnParticle(Math.random() * 10_000)
+                        }
+                        spawnParticlesAtTime = Number.MAX_SAFE_INTEGER
+                    }
+                },
+            })
+        )
+
         e.addComponent(
             new Interactable(
                 interactPos,
-                () => wl.playerUseTeleporter(destinationUUID, id),
+                () => {
+                    wl.playerUseTeleporter(destinationUUID, id)
+                    spawnParticlesAtTime = WorldTime.instance.time + 1_000
+                },
                 new Point(0, -TILE_SIZE)
             )
         )
 
         // TODO: pre-populate the area so particles are present when entered
-        e.addComponent(
-            new RepeatedInvoker(() => {
-                // create a particle
-                const pos = new Point(5, 3)
-                    .apply((n) => Math.random() * n * TILE_SIZE)
-                    .plusY(-2 * TILE_SIZE) // align with top
-                Particles.instance.emitParticle(
-                    Lists.oneOf([Color.BLACK, Color.TAUPE_1, Color.TAUPE_2]),
-                    pos,
-                    1_000_000_000,
-                    10_000,
-                    (t) => pt(0, t * 0.004),
-                    pt(Math.random() > 0.5 ? 1 : 2)
-                )
-
-                return 500 + Math.random() * 500
-            })
-        )
+        e.addComponent(new RepeatedInvoker(() => spawnParticle()))
 
         return e.addComponent(new ElementComponent(ElementType.MINE_EXIT, pos, () => data))
     }
