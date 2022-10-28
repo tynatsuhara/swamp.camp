8da75f62f563e6db3d7700f515ec30d0cb5996c1
Tyler Bonnell <tbonnell@uw.edu>
2022-10-28 13:56:01 -0700

    syncvar

--- src/app_client.ts
+++ src/app_client.ts
@@ -1,8 +1,13 @@
-import { joinRoom } from "trystero"
+import { isHost, room, syncData } from "./online/OnlineUtils"
 
-const room = joinRoom(
-    { appId: "92fe373a-2446-426a-8b8d-28b51bb30b01" },
-    "30908535-9bba-4e21-8a68-3384b7cd604f"
-)
+const exampleSyncData = syncData<{ health: number }>("p1-health", { health: 5 }, () => {
+    console.log(exampleSyncData.health)
+})
 
-room.onPeerJoin((peerId) => console.log(`${peerId} joined`))
+room.onPeerJoin((peerId) => {
+    if (isHost) {
+        console.log(`${peerId} joined`)
+        exampleSyncData.health--
+        console.log(exampleSyncData)
+    }
+})
