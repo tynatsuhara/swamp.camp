35af91220e6122b0ce08b5d090a183f018f272e4
Tyler Bonnell <tbonnell@uw.edu>
2022-11-02 13:17:14 -0700

    delete redundant font

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ declare global {
 Promise.all([
     // load all required assets
     assets.loadImageFiles(getFilesToLoadForMainMenu()),
-    assets.loadFont(TEXT_FONT, "url(/press-start-2p.woff2)"),
+    assets.loadFont(TEXT_FONT, "url(/fonts/press-start-2p.woff2)"),
 ]).then(() => {
     // start the engine (vroom vroom)
     Engine.start(new SwampCampGame(), <HTMLCanvasElement>document.getElementById("canvas"))
