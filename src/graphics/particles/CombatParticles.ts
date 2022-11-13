dffac21f44a488f0e7f6a8b68cddaaa69bdcc794
Tyler Natsuhara <ty@ty.pizza>
2022-11-13 14:03:45 -0800

    tweak particles

--- src/graphics/particles/CombatParticles.ts
+++ src/graphics/particles/CombatParticles.ts
@@ -7,11 +7,12 @@ import { Particles } from "./Particles"
 
 export const emitBlockParticles = syncFn(
     "blkprtcle",
+    // MPTODO: Sync dude UUID instead
     (facingMultiplier: number, standingPosition: PointValue) => {
         console.log("block particles")
         const particles = 5 + Math.random() * 5
         for (let i = 0; i < particles; i++) {
-            const lifeSpan = 120
+            const lifeSpan = 150
 
             let offset = Point.ZERO.randomCircularShift(1)
             offset = pt(Math.abs(offset.x) * facingMultiplier, offset.y)
@@ -19,10 +20,10 @@ export const emitBlockParticles = syncFn(
             Particles.instance.emitComplexParticle(
                 Lists.oneOf([Color.WHITE, Color.RED_5]),
                 () => offset.plus(standingPosition).plus(pt(facingMultiplier * 4, -7)),
-                () => standingPosition.y + 5,
+                () => standingPosition.y + 1,
                 lifeSpan,
                 (t) => offset.times(t * 0.08),
-                pt(1)
+                pt(Math.random() > 0.5 ? 1 : 2)
             )
         }
     }
