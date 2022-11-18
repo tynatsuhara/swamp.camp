56f5f1e1d4da174bc71784b2467d2c76a71bff3c
Tyler Natsuhara <ty@ty.pizza>
2022-11-17 16:29:22 -0800

    pt->ptval

--- src/world/elements/ElementUtils.ts
+++ src/world/elements/ElementUtils.ts
@@ -1,11 +1,12 @@
-import { Point } from "brigsby/dist"
+import { Point, pt } from "brigsby/dist"
+import { PointValue } from "brigsby/dist/Point"
 
 export const ElementUtils = {
-    rectPoints: (position: Point, dimensions: Point): Point[] => {
+    rectPoints: (position: PointValue, dimensions: Point): Point[] => {
         const result = []
         for (let x = 0; x < dimensions.x; x++) {
             for (let y = 0; y < dimensions.y; y++) {
-                result.push(position.plus(new Point(x, y)))
+                result.push(pt(position.x + x, position.y + y))
             }
         }
         return result
