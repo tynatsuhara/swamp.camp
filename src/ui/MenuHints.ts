29b5ee7d84ef07c73d2df9b22d795dbdf008c49b
Tyler Natsuhara <ty@ty.pizza>
2022-12-30 16:38:12 -0800

    add skeleton menuhints

--- src/ui/MenuHints.ts
+++ src/ui/MenuHints.ts
@@ -2,8 +2,12 @@ import { Component } from "brigsby/dist/Component"
 import { Entity } from "brigsby/dist/Entity"
 import { controls } from "../Controls"
 import { Singletons } from "../Singletons"
+import { CraftingMenu } from "./CraftingMenu"
+import { InventoryDisplay } from "./InventoryDisplay"
 import { PauseMenu } from "./PauseMenu"
+import { PlaceElementDisplay } from "./PlaceElementDisplay"
 import { Tooltip } from "./Tooltip"
+import { TradeMenu } from "./TradeMenu"
 
 export class MenuHints extends Component {
     private readonly e = new Entity([this])
@@ -20,12 +24,22 @@ export class MenuHints extends Component {
     update(): void {
         const controlsToShow: string[] = []
 
-        const showControl = (buttonString: string, action: string) => {
+        const show = (buttonString: string, action: string) => {
             controlsToShow.push(buttonString + " " + action)
         }
+        const showClose = () => show(controls.getCloseMenuButtonString(), "close")
 
-        if (PauseMenu.instance.isOpen) {
-            showControl(controls.getCloseMenuButtonString(), "close")
+        // TODO add all controls
+        if (InventoryDisplay.instance.isOpen) {
+            showClose()
+        } else if (PlaceElementDisplay.instance.isOpen) {
+            showClose()
+        } else if (PauseMenu.instance.isOpen) {
+            showClose()
+        } else if (CraftingMenu.instance.isOpen) {
+            showClose()
+        } else if (TradeMenu.instance.isOpen) {
+            showClose()
         }
 
         if (controlsToShow.length > 0) {
