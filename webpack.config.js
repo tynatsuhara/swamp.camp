ff6055e5c4491391597d14852a67a69adf3939aa
Tyler Natsuhara <ty@ty.pizza>
2023-03-28 21:26:27 -0700

    add check script

--- webpack.config.js
+++ webpack.config.js
@@ -8,56 +8,65 @@ const workers = fs
     .map(({ name }) => name.replace(".ts", ""))
     .map((name) => [`workers/${name}`, `./src/workers/${name}.ts`])
 
-module.exports = (_, argv) => ({
-    entry: {
-        app: "./src/app.ts",
-        ...Object.fromEntries(workers),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
+module.exports = (_, argv) => {
+    const plugins = []
+
+    if (process.env.CYCLE_CHECK) {
+        plugins.push(
+            new CircularDependencyPlugin({
+                // exclude detection of files based on a RegExp
                 exclude: /node_modules/,
+                // include specific files based on a RegExp
+                include: /src/,
+                // add errors to webpack instead of warnings
+                failOnError: false,
+            })
+        )
+    }
+
+    return {
+        entry: {
+            app: "./src/app.ts",
+            ...Object.fromEntries(workers),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+            symlinks: false,
+        },
+        output: {
+            filename: "[name].js",
+            path: path.resolve(__dirname, "static"),
+        },
+        devtool:
+            argv.mode === "development" ? "eval-cheap-module-source-map" : "nosources-source-map",
+        devServer: {
+            allowedHosts: "all",
+            https: false,
+            compress: true,
+            hot: false,
+            liveReload: false,
+            open: false,
+            port: 8000,
+            client: {
+                webSocketURL: "auto://0.0.0.0:0/ws",
             },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-        symlinks: false,
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "static"),
-    },
-    devtool: argv.mode === "development" ? "eval-cheap-module-source-map" : "nosources-source-map",
-    devServer: {
-        allowedHosts: "all",
-        https: false,
-        compress: true,
-        hot: false,
-        liveReload: false,
-        open: false,
-        port: 8000,
-        client: {
-            webSocketURL: "auto://0.0.0.0:0/ws",
+            static: {
+                directory: path.join(__dirname, "static"),
+            },
         },
-        static: {
-            directory: path.join(__dirname, "static"),
+        stats: {
+            assets: false,
+            modules: false,
         },
-    },
-    stats: {
-        assets: false,
-        modules: false,
-    },
-    plugins: [
-        new CircularDependencyPlugin({
-            // exclude detection of files based on a RegExp
-            exclude: /node_modules/,
-            // include specific files based on a RegExp
-            include: /src/,
-            // add errors to webpack instead of warnings
-            failOnError: false,
-        }),
-    ],
-})
+        plugins,
+    }
+}
