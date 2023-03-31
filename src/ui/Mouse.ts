d728f7d29c53c72aff180e493862d89c6776facf
Tyler Natsuhara <ty@ty.pizza>
2023-03-31 16:48:37 -0700

    mouse scaling

--- src/ui/Mouse.ts
+++ src/ui/Mouse.ts
@@ -1,3 +1,6 @@
+import { renderer } from "brigsby/dist/renderer/Renderer"
+import { ZOOM } from "../SwampCampGame"
+
 export const Mouse = {
     lock: () => {
         document.body.requestPointerLock()
@@ -9,10 +12,21 @@ export const Mouse = {
     },
 
     hide: () => {
-        document.body.style["cursor"] = "none"
+        document.body.style.cursor = "none"
     },
 
     show: () => {
-        document.body.style["cursor"] = "url(cursor_3x.png), default"
+        const scale = ZOOM * renderer.getScale()
+        // converted from png with https://codepen.io/shshaw/pen/XbxvNj
+        const svg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="${9 * scale}" height="${
+            12 * scale
+        }" viewBox="0 -0.5 9 12" shape-rendering="crispEdges">
+<path stroke="#d3bfa9" d="M0 0h2M0 1h1M2 1h1M0 2h1M3 2h1M0 3h1M4 3h1M0 4h1M5 4h1M0 5h1M6 5h1M0 6h1M7 6h1M0 7h1M8 7h1M0 8h1M4 8h5M0 9h1M3 9h1M0 10h1M2 10h1M0 11h2" />
+<path stroke="#5f2d56" d="M1 1h1M1 2h2M1 3h3M1 4h4M1 5h5M1 6h6M1 7h7M1 8h3M1 9h2M1 10h1" />
+</svg>`
+        document.body.style.cursor = `url(data:image/svg+xml;utf-8,${encodeURIComponent(
+            svg
+        )}), default`
     },
 }
