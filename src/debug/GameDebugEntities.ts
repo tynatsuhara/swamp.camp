f8a06fef803f34ef87ea766b2755181a78f0d4c9
Tyler Natsuhara <ty@ty.pizza>
2023-05-26 11:49:28 -0700

    split out debug entities

---
+++ src/debug/GameDebugEntities.ts
@@ -0,0 +1,186 @@
+import { debug, Entity, Point } from "brigsby/dist"
+import {
+    BasicRenderComponent,
+    EllipseRender,
+    LineRender,
+    RectRender,
+    RenderMethod,
+} from "brigsby/dist/renderer"
+import { DudeType } from "../characters/DudeType"
+import { player } from "../characters/player"
+import { controls } from "../Controls"
+import { Camera } from "../cutscenes/Camera"
+import { TILE_SIZE } from "../graphics/Tilesets"
+import { ButtonsMenu, OptionButton } from "../ui/ButtonsMenu"
+import { Color } from "../ui/Color"
+import { Elements } from "../world/elements/Elements"
+import { here } from "../world/locations/LocationManager"
+import { ProfilerData } from "./ProfilerData"
+import { spawnMenu } from "./SpawnMenu"
+
+export const getUiSpaceDebugEntities = (): Entity[] => {
+    const result: Entity[] = []
+
+    if (spawnMenu.isOpen) {
+        const selectedType = spawnMenu.getSelectedType()
+        const pageStart = spawnMenu.page * spawnMenu.pageSize
+        const dudeTypeButtons: OptionButton[] = [
+            selectedType,
+            ...spawnMenu.types.filter((t) => t !== selectedType),
+        ]
+            .slice(pageStart, pageStart + spawnMenu.pageSize)
+            .map((type, index) => ({
+                text:
+                    type === selectedType
+                        ? `[SELECTED] ${DudeType[type]}`
+                        : `SPAWN ${DudeType[type]}`,
+                fn: () => {
+                    spawnMenu.setSelectedType(type)
+                    console.log(`press [O] to spawn ${DudeType[type]} at mouse position`)
+                    spawnMenu.isOpen = false
+                },
+                buttonColor: "white",
+                textColor: Color.WHITE,
+                hoverColor: Color.RED_2,
+            }))
+
+        const nextPageButton: OptionButton = {
+            text: "NEXT",
+            fn: () => {
+                spawnMenu.page++
+                if (spawnMenu.page * spawnMenu.pageSize > spawnMenu.types.length) {
+                    spawnMenu.page = 0
+                }
+            },
+            buttonColor: "white",
+            textColor: Color.WHITE,
+            hoverColor: Color.RED_2,
+        }
+
+        const buttons = [...dudeTypeButtons, nextPageButton]
+
+        const { entity: menuEntity } = ButtonsMenu.render(
+            "spawn",
+            "white",
+            buttons,
+            Camera.instance.dimensions.div(2)
+        )
+        result.push(menuEntity)
+
+        if (controls.isCloseMenuButtonDown()) {
+            spawnMenu.isOpen = false
+        }
+    }
+
+    if (debug.showProfiler) {
+        result.push(new Entity([new ProfilerData()]))
+    }
+
+    return result
+}
+
+export const getWorldSpaceDebugEntity = (): Entity => {
+    if (!player()) {
+        return
+    }
+
+    const e = new Entity()
+
+    if (debug.showGrid) {
+        const base = player().tile
+        const lines: RenderMethod[] = []
+        const gridRange = 50
+
+        // vertical lines
+        for (let i = -gridRange; i < gridRange; i++) {
+            const top = base
+                .times(TILE_SIZE)
+                .plusX(i * TILE_SIZE)
+                .plusY(-gridRange * TILE_SIZE)
+            lines.push(new LineRender(top, top.plusY(2 * gridRange * TILE_SIZE), "#0000FF7E"))
+        }
+
+        // horizontal lines
+        for (let i = -gridRange; i < gridRange; i++) {
+            const left = base
+                .times(TILE_SIZE)
+                .plusX(-gridRange * TILE_SIZE)
+                .plusY(i * TILE_SIZE)
+            lines.push(new LineRender(left, left.plusX(2 * gridRange * TILE_SIZE), "#0000FF7E"))
+        }
+
+        e.addComponent(new BasicRenderComponent(...lines))
+    }
+
+    if (debug.showTeleporters) {
+        const pts = here().getTeleporterLocations()
+        const renders = pts.map(
+            (pt) =>
+                new EllipseRender({
+                    depth: Number.MAX_SAFE_INTEGER,
+                    position: pt.plus(new Point(-2, -2)),
+                    dimensions: new Point(4, 4),
+                })
+        )
+
+        e.addComponent(new BasicRenderComponent(...renders))
+    }
+
+    if (debug.showNavMesh) {
+        const pts = here().getOccupiedSpots()
+        const renders = pts.map(
+            (pt) =>
+                new RectRender({
+                    depth: Number.MAX_SAFE_INTEGER,
+                    position: pt.times(TILE_SIZE),
+                    dimensions: new Point(TILE_SIZE, TILE_SIZE),
+                    color: "#0000FF7E",
+                })
+        )
+
+        e.addComponent(new BasicRenderComponent(...renders))
+    }
+
+    if (debug.showGroundTiles) {
+        const pts = here().getAllGroundSpots()
+        const renders = pts.map(
+            (pt) =>
+                new RectRender({
+                    depth: Number.MAX_SAFE_INTEGER,
+                    position: pt.times(TILE_SIZE),
+                    dimensions: new Point(TILE_SIZE, TILE_SIZE),
+                    color: "#00FFFF7E",
+                })
+        )
+
+        e.addComponent(new BasicRenderComponent(...renders))
+    }
+
+    if (debug.showElementGrid) {
+        const renders = here()
+            .getElements()
+            .map(
+                (el) =>
+                    new RectRender({
+                        depth: Number.MAX_SAFE_INTEGER,
+                        position: el.pos.times(TILE_SIZE),
+                        dimensions: Elements.instance
+                            .getElementFactory(el.type)
+                            .dimensions.times(TILE_SIZE),
+                        color: "#A000A07E",
+                    })
+            )
+
+        e.addComponent(new BasicRenderComponent(...renders))
+    }
+
+    // TODO figure out a nice way to draw pixelated lines for bomb tossing
+    // e.addComponent(
+    //     new ThrowableArc(
+    //         Player.instance.dude.standingPosition,
+    //         controls.getWorldSpaceMousePos()
+    //     )
+    // )
+
+    return e
+}
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,34 +1,22 @@
 import { debug, Entity, Point, Scene, UpdateViewsContext } from "brigsby/dist"
 import { CollisionEngine, collisionEngine } from "brigsby/dist/collision"
-import {
-    BasicRenderComponent,
-    EllipseRender,
-    LineRender,
-    RectRender,
-    RenderMethod,
-} from "brigsby/dist/renderer"
 import { Dude } from "../characters/Dude"
 import { DudeFactory } from "../characters/DudeFactory"
 import { DudeSpawner } from "../characters/DudeSpawner"
 import { DudeType } from "../characters/DudeType"
 import { NPC } from "../characters/NPC"
-import { player } from "../characters/player"
 import { controls } from "../Controls"
 import { Camera } from "../cutscenes/Camera"
 import { CutsceneManager } from "../cutscenes/CutsceneManager"
 import { IntroCutscene } from "../cutscenes/IntroCutscene"
 import { TextOverlayManager } from "../cutscenes/TextOverlayManager"
 import { DevControls } from "../debug/DevControls"
-import { ProfilerData } from "../debug/ProfilerData"
-import { spawnMenu } from "../debug/SpawnMenu"
+import { getUiSpaceDebugEntities, getWorldSpaceDebugEntity } from "../debug/GameDebugEntities"
 import { Particles } from "../graphics/particles/Particles"
 import { TILE_SIZE } from "../graphics/Tilesets"
 import { DroppedItem } from "../items/DroppedItem"
 import { ZOOM } from "../SwampCampGame"
-import { ButtonsMenu, OptionButton } from "../ui/ButtonsMenu"
-import { Color } from "../ui/Color"
 import { UIStateManager } from "../ui/UIStateManager"
-import { Elements } from "../world/elements/Elements"
 import { EventQueue } from "../world/events/EventQueue"
 import { QueuedEventType } from "../world/events/QueuedEvent"
 import { GroundRenderer } from "../world/ground/GroundRenderer"
@@ -83,11 +71,11 @@ export class GameScene extends Scene {
             updateViewsContext.elapsedTimeMillis
         )
 
-        const uiEntities = [...this.getUiSpaceDebugEntities()]
+        const uiEntities = [...getUiSpaceDebugEntities()]
 
         const gameEntities: Entity[] = [
             CutsceneManager.instance.getEntity(),
-            this.getWorldSpaceDebugEntity(),
+            getWorldSpaceDebugEntity(),
             new Entity([new DevControls()]),
         ]
 
@@ -131,172 +119,5 @@ export class GameScene extends Scene {
                 entities: uiEntities,
             },
         ]
-    }
-
-    private getWorldSpaceDebugEntity(): Entity {
-        if (!player()) {
-            return
-        }
-
-        const e = new Entity()
-
-        if (debug.showGrid) {
-            const base = player().tile
-            const lines: RenderMethod[] = []
-            const gridRange = 50
-
-            // vertical lines
-            for (let i = -gridRange; i < gridRange; i++) {
-                const top = base
-                    .times(TILE_SIZE)
-                    .plusX(i * TILE_SIZE)
-                    .plusY(-gridRange * TILE_SIZE)
-                lines.push(new LineRender(top, top.plusY(2 * gridRange * TILE_SIZE), "#0000FF7E"))
-            }
-
-            // horizontal lines
-            for (let i = -gridRange; i < gridRange; i++) {
-                const left = base
-                    .times(TILE_SIZE)
-                    .plusX(-gridRange * TILE_SIZE)
-                    .plusY(i * TILE_SIZE)
-                lines.push(new LineRender(left, left.plusX(2 * gridRange * TILE_SIZE), "#0000FF7E"))
-            }
-
-            e.addComponent(new BasicRenderComponent(...lines))
-        }
-
-        if (debug.showTeleporters) {
-            const pts = here().getTeleporterLocations()
-            const renders = pts.map(
-                (pt) =>
-                    new EllipseRender({
-                        depth: Number.MAX_SAFE_INTEGER,
-                        position: pt.plus(new Point(-2, -2)),
-                        dimensions: new Point(4, 4),
-                    })
-            )
-
-            e.addComponent(new BasicRenderComponent(...renders))
-        }
-
-        if (debug.showNavMesh) {
-            const pts = here().getOccupiedSpots()
-            const renders = pts.map(
-                (pt) =>
-                    new RectRender({
-                        depth: Number.MAX_SAFE_INTEGER,
-                        position: pt.times(TILE_SIZE),
-                        dimensions: new Point(TILE_SIZE, TILE_SIZE),
-                        color: "#0000FF7E",
-                    })
-            )
-
-            e.addComponent(new BasicRenderComponent(...renders))
-        }
-
-        if (debug.showGroundTiles) {
-            const pts = here().getAllGroundSpots()
-            const renders = pts.map(
-                (pt) =>
-                    new RectRender({
-                        depth: Number.MAX_SAFE_INTEGER,
-                        position: pt.times(TILE_SIZE),
-                        dimensions: new Point(TILE_SIZE, TILE_SIZE),
-                        color: "#00FFFF7E",
-                    })
-            )
-
-            e.addComponent(new BasicRenderComponent(...renders))
-        }
-
-        if (debug.showElementGrid) {
-            const renders = here()
-                .getElements()
-                .map(
-                    (el) =>
-                        new RectRender({
-                            depth: Number.MAX_SAFE_INTEGER,
-                            position: el.pos.times(TILE_SIZE),
-                            dimensions: Elements.instance
-                                .getElementFactory(el.type)
-                                .dimensions.times(TILE_SIZE),
-                            color: "#A000A07E",
-                        })
-                )
-
-            e.addComponent(new BasicRenderComponent(...renders))
-        }
-
-        // TODO figure out a nice way to draw pixelated lines for bomb tossing
-        // e.addComponent(
-        //     new ThrowableArc(
-        //         Player.instance.dude.standingPosition,
-        //         controls.getWorldSpaceMousePos()
-        //     )
-        // )
-
-        return e
-    }
-
-    private getUiSpaceDebugEntities(): Entity[] {
-        const result: Entity[] = []
-
-        if (spawnMenu.isOpen) {
-            const selectedType = spawnMenu.getSelectedType()
-            const pageStart = spawnMenu.page * spawnMenu.pageSize
-            const dudeTypeButtons: OptionButton[] = [
-                selectedType,
-                ...spawnMenu.types.filter((t) => t !== selectedType),
-            ]
-                .slice(pageStart, pageStart + spawnMenu.pageSize)
-                .map((type, index) => ({
-                    text:
-                        type === selectedType
-                            ? `[SELECTED] ${DudeType[type]}`
-                            : `SPAWN ${DudeType[type]}`,
-                    fn: () => {
-                        spawnMenu.setSelectedType(type)
-                        console.log(`press [O] to spawn ${DudeType[type]} at mouse position`)
-                        spawnMenu.isOpen = false
-                    },
-                    buttonColor: "white",
-                    textColor: Color.WHITE,
-                    hoverColor: Color.RED_2,
-                }))
-
-            const nextPageButton: OptionButton = {
-                text: "NEXT",
-                fn: () => {
-                    spawnMenu.page++
-                    if (spawnMenu.page * spawnMenu.pageSize > spawnMenu.types.length) {
-                        spawnMenu.page = 0
-                    }
-                },
-                buttonColor: "white",
-                textColor: Color.WHITE,
-                hoverColor: Color.RED_2,
-            }
-
-            const buttons = [...dudeTypeButtons, nextPageButton]
-
-            const { entity: menuEntity } = ButtonsMenu.render(
-                "spawn",
-                "white",
-                buttons,
-                Camera.instance.dimensions.div(2)
-            )
-            result.push(menuEntity)
-
-            if (controls.isCloseMenuButtonDown()) {
-                spawnMenu.isOpen = false
-            }
-        }
-
-        if (debug.showProfiler) {
-            result.push(new Entity([new ProfilerData()]))
-        }
-
-        return result
     }
 }
