7264156e3763ce6694dff2c27a853fb65428ff94
Tyler Natsuhara <ty@ty.pizza>
2023-11-09 17:03:11 -0800

    add mimic and spooky visitor sounds

--- src/audio/VocalSounds.ts
+++ src/audio/VocalSounds.ts
@@ -1,51 +1,79 @@
+// faction sounds:
 /* DudeFaction {
     VILLAGERS, // TODO (including player!)
-    CLERGY, // TODO
     ORCS, // sounds: rpg/npc/ogre
     UNDEAD, // TODO
     DEMONS, // sounds: rpg/npc/shade
     SHROOMS, // sounds: lots of options in rpg/npc/* that could work here
     CENTAURS, // TODO
-    GNOLLS, // TODO
+    GNOLLS + WOLVES, // TODO
     BEARS, // TODO
-    WOLVES, // TODO sounds: gumball
     AQUATIC, // TODO sounds: lots of options in rpg/npc/* that could work here
+} */
+
+// special cases:
+/* DudeType {
+    PLAYER,
+    SPOOKY_VISITOR,
+    GUMBALL,
+    ONION,
+    KNIGHT,
+    FOREST_GUARDIAN,
+    MIMIC,
+    SKELETON,
 } */
 
 import { Lists } from "brigsby/dist/util/Lists"
 import { Dude } from "../characters/Dude"
 import { DudeFaction } from "../characters/DudeFactory"
+import { DudeType } from "../characters/DudeType"
 import { loadAudio } from "./DeferLoadAudio"
 import { Sounds } from "./Sounds"
 
-const DEMON_SOUNDS = loadAudio(Lists.range(1, 16).map((i) => `audio/rpg/NPC/shade/shade${i}.wav`))
-const ORC_SOUNDS = loadAudio(Lists.range(1, 6).map((i) => `audio/rpg/NPC/ogre/ogre${i}.wav`))
+const { range, oneOf } = Lists
 
-const play = (d: Dude, sound: string, volume: number = 0.2) => {
-    Sounds.playAtPoint(sound, volume, d.standingPosition)
+const DEMON_SOUNDS = loadAudio(range(1, 16).map((i) => `audio/rpg/NPC/shade/shade${i}.wav`))
+const ORC_SOUNDS = loadAudio(range(1, 6).map((i) => `audio/rpg/NPC/ogre/ogre${i}.wav`))
+const SPOOKY_SOUNDS = loadAudio(range(1, 3).map((i) => `audio/m1/spooky_visitor_idle${i}.ogg`))
+const MIMIC_SOUNDS = loadAudio(range(1, 3).map((i) => `audio/m1/mimic${i}.ogg`))
+
+const play = (
+    d: Dude,
+    sound: string,
+    { volume = 0.2, chance = 1 }: { volume?: number; chance?: number } = {}
+) => {
+    if (Math.random() < chance) {
+        Sounds.playAtPoint(sound, volume, d.standingPosition)
+    }
 }
 
 const ambient = (d: Dude) => {
     if (d.factions.includes(DudeFaction.DEMONS)) {
-        play(d, Lists.oneOf(DEMON_SOUNDS))
+        play(d, oneOf(DEMON_SOUNDS))
     } else if (d.factions.includes(DudeFaction.ORCS)) {
-        play(d, Lists.oneOf(ORC_SOUNDS))
+        play(d, oneOf(ORC_SOUNDS))
+    } else if (d.type === DudeType.SPOOKY_VISITOR) {
+        play(d, oneOf(SPOOKY_SOUNDS), { volume: 1, chance: 0.2 })
     }
 }
 
 const attack = (d: Dude) => {
     if (d.factions.includes(DudeFaction.DEMONS)) {
-        play(d, Lists.oneOf(DEMON_SOUNDS))
+        play(d, oneOf(DEMON_SOUNDS))
     } else if (d.factions.includes(DudeFaction.ORCS)) {
-        play(d, Lists.oneOf(ORC_SOUNDS))
+        play(d, oneOf(ORC_SOUNDS))
+    } else if (d.type === DudeType.MIMIC) {
+        play(d, oneOf(MIMIC_SOUNDS), { volume: 0.75 })
     }
 }
 
 const damage = (d: Dude) => {
     if (d.factions.includes(DudeFaction.DEMONS)) {
-        play(d, Lists.oneOf(DEMON_SOUNDS))
+        play(d, oneOf(DEMON_SOUNDS))
     } else if (d.factions.includes(DudeFaction.ORCS)) {
-        play(d, Lists.oneOf(ORC_SOUNDS))
+        play(d, oneOf(ORC_SOUNDS))
+    } else if (d.type === DudeType.SPOOKY_VISITOR) {
+        play(d, oneOf(SPOOKY_SOUNDS), { volume: 1 })
     }
 }
 
